8f21802cd2b8c791ac15ea1304ff83f96f43f96c
will4ntz <wiliam-m-@hotmail.com>
2024-12-22 21:43:02 -0300

    :sparkles: created core class

--- include/ocrcore.h
+++ include/ocrcore.h
@@ -5,12 +5,57 @@
 #ifndef OCRCORE_H
 #define OCRCORE_H
 
+#include "platereader.h"
+#include "platefinder.h"
+#include <regex>
 
 
-class ocrcore {
-
+struct CharResult {
+    char letter;
+    float confidence;
+    cv::Rect rect;
 };
 
+struct OcrResult {
+    std::string plateString{};
+    bool isMercosul{};
+    float confidence{};
+    cv::Rect rect;
+    std::vector<CharResult> chars{};
+};
+
+
+class OcrCore {
+private:
+    PlateFinder *plateFinder{};
+    PlateReader *plateReader{};
+
+    const cv::Size inputPlateFinderModel = cv::Size(320, 320);
+    const cv::Size inputPlateReaderModel = cv::Size(320, 320);
+
+    std::vector<Detection> performPlateFinder(const cv::Mat &frame) const;
+    std::vector<Detection> performPlateReader(const cv::Mat &frame) const;
+
+    std::string assembleStrPlate(const std::vector<Detection> &chars) const;
+
+    static std::optional<bool> validateLicensePlate(const std::string &plateStr);
+    static std::vector<Detection> buildPlate(const std::vector<Detection> &chars, cv::Mat &image_roi);
+
+
+public:
+    OcrCore();
+    OcrCore(const std::string &plateModelPath, const std::string &plateReaderPath);
+    ~OcrCore();
+
+    //td::vector<std::string> getOcr(const cv::Mat &frame) const;
+    std::vector<OcrResult> getOcr(const cv::Mat &frame) const;
+
+    // OCR UTILS
+    static bool compareByLength(const Detection& a, const Detection& b);
+    static bool compareByConfidence(const Detection& a, const Detection& b);
+    static bool compareByHeight(const Detection& a, const Detection& b);
+    static void checkBbox(const cv::Mat& frame, cv::Rect& bbox);
+};
 
 
 #endif //OCRCORE_H
--- src/ocrcore.cpp
+++ src/ocrcore.cpp
@@ -3,3 +3,154 @@
 //
 
 #include "../include/ocrcore.h"
+
+OcrCore::OcrCore() {
+    plateFinder = new PlateFinder("");
+    plateReader = new PlateReader("");
+}
+OcrCore::OcrCore(const std::string &plateModelPath, const std::string &plateReaderPath) {
+    plateFinder = new PlateFinder(plateModelPath);
+    plateReader = new PlateReader(plateReaderPath);
+}
+
+OcrCore::~OcrCore() = default;
+
+std::vector<OcrResult> OcrCore::getOcr(const cv::Mat &frame) const {
+    std::vector<OcrResult> result{};
+
+    auto plates = this->performPlateFinder(frame);
+
+    for (auto &plate : plates) {
+        plate.bbox.x = std::max(0, plate.bbox.x - plate.bbox.width / 5);
+        plate.bbox.y = std::max(0, plate.bbox.y - static_cast<int>(plate.bbox.height / 1.5));
+        plate.bbox.width += plate.bbox.width * 2 / 5;
+        plate.bbox.height += plate.bbox.height * (2 / 1.5);
+        plate.bbox.height = std::min(plate.bbox.height, frame.rows - plate.bbox.y);
+        plate.bbox.width = std::min(plate.bbox.width, frame.cols - plate.bbox.x);
+
+        OcrCore::checkBbox(frame, plate.bbox);
+        cv::Mat image_roi = frame(plate.bbox).clone();
+
+        std::vector<Detection> chars = this->performPlateReader(image_roi);
+
+        auto plateChars = this->buildPlate(chars, image_roi);
+
+        auto plateStr = this->assembleStrPlate(plateChars);
+
+        auto validatedPlate = this->validateLicensePlate(plateStr);
+
+        if (validatedPlate.has_value()) {
+
+            std::vector<CharResult> charsResults;
+
+            for (size_t i = 0; i < plateChars.size(); i++) {
+                charsResults.push_back({
+                    plateStr[i],
+                    plateChars[i].confidence,
+                    plateChars[i].bbox,
+                });
+            }
+
+            result.push_back({
+                plateStr,
+                validatedPlate.value(),
+                plate.confidence,
+                plate.bbox,
+                charsResults
+                }
+            );
+        }
+    }
+
+    return result;
+}
+
+std::vector<Detection> OcrCore::performPlateFinder(const cv::Mat &frame) const {
+    const cv::Mat processedImage = ImageOperator::preprocess(frame, inputPlateFinderModel);
+    const std::vector<float> blobImage = ImageOperator::toBlob(processedImage);
+    float* output_tensor = plateFinder->infer(blobImage);
+    return plateFinder->postProcess(output_tensor);
+}
+
+std::vector<Detection> OcrCore::performPlateReader(const cv::Mat &frame) const {
+    const cv::Mat processedImage = ImageOperator::preprocess(frame, inputPlateReaderModel);
+    const std::vector<float> blobImage = ImageOperator::toBlob(processedImage);
+    float* output_tensor = plateReader->infer(blobImage);
+    return plateReader->postProcess(output_tensor);
+}
+
+std::vector<Detection> OcrCore::buildPlate(const std::vector<Detection> &chars, cv::Mat &image_roi) {
+    if (chars.size() < 7) {
+        return {};
+    }
+
+    std::vector<Detection> plateTmp(chars.begin(), chars.begin() + std::min(7, (int)chars.size()));
+
+    if (image_roi.cols < image_roi.rows * 1.4) {
+        std::sort(plateTmp.begin(), plateTmp.end(), compareByHeight);
+
+        std::vector<Detection> topRow(plateTmp.begin(), plateTmp.begin() + 3);
+        std::vector<Detection> bottomRow(plateTmp.end() - 4, plateTmp.end());
+
+        std::sort(topRow.begin(), topRow.end(), compareByLength);
+        std::sort(bottomRow.begin(), bottomRow.end(), compareByLength);
+
+        plateTmp.insert(plateTmp.end(), topRow.begin(), topRow.end());
+        plateTmp.insert(plateTmp.end(), bottomRow.begin(), bottomRow.end());
+    } else {
+        std::sort(plateTmp.begin(), plateTmp.end(), compareByLength);
+    }
+
+    return plateTmp;
+}
+
+std::string OcrCore::assembleStrPlate(const std::vector<Detection> &chars) const {
+    if (chars.empty()) {
+        return {};
+    }
+
+    std::string plateStr;
+    for (const auto &c : chars) {
+        plateStr += plateReader->getClasses()[static_cast<int>(c.classId)];
+    }
+    return plateStr;
+}
+
+std::optional<bool> OcrCore::validateLicensePlate(const std::string &plateStr) {
+    if (plateStr.empty()) {
+        return std::nullopt;
+    }
+
+    static const std::regex oldFormat("^[A-Z]{3}\\d{4}$");
+    static const std::regex newFormat("^[A-Z]{3}\\d[A-Z]\\d{2}$");
+
+    if (std::regex_match(plateStr, newFormat)) {
+        return true; // Mercosul
+    }
+
+    if (std::regex_match(plateStr, oldFormat)) {
+        return false; // Normal
+    }
+
+    return std::nullopt; // Inválido
+}
+
+inline bool OcrCore::compareByLength(const Detection &a, const Detection &b) {
+    return a.bbox.x < b.bbox.x;
+}
+
+inline bool OcrCore::compareByConfidence(const Detection &a, const Detection &b) {
+    return a.confidence > b.confidence;
+}
+
+inline bool OcrCore::compareByHeight(const Detection &a, const Detection &b) {
+    return a.bbox.y < b.bbox.y;
+}
+
+inline void OcrCore::checkBbox(const cv::Mat &frame, cv::Rect &bbox) {
+    const int x = std::max(bbox.x, 0);
+    const int y = std::max(bbox.y, 0);
+    const int width = std::min(bbox.width, frame.cols - x);
+    const int height = std::min(bbox.height, frame.rows - y);
+    bbox = (width <= 0 || height <= 0) ? cv::Rect(1,1,1,1) : cv::Rect(x, y, width, height);
+}
