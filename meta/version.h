43da0f4c21e0d776c944fc17411a7778afeec972
will4ntz <wiliam-m-@hotmail.com>
2024-12-22 22:09:42 -0300

    :bento: added templates

---
+++ meta/version.h
@@ -0,0 +1,13 @@
+//
+// Created by willian on 12/22/24.
+//
+
+#ifndef VERSION_H
+#define VERSION_H
+
+
+#define GIT_COMMIT_HASH     "@GIT_COMMIT_HASH@"
+#define GIT_BRANCH          "@GIT_BRANCH@"
+#define ANPR_VERSION        "@ANPR_VERSION@"
+
+#endif //VERSION_H
---
+++ meta/weights.h
@@ -0,0 +1,10 @@
+//
+// Created by willian on 12/22/24.
+//
+
+#ifndef WEIGHTS_H
+#define WEIGHTS_H
+
+@WEIGHTS_HEADER@
+
+#endif //WEIGHTS_H
